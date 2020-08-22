a9339c949d17130ebb6c377cb63a7fa938084238
neha_karmakar <neha.karmakar@embibe.com>
2020-08-22 16:15:59 +0530

    Updates

---
+++ precache-manifest.36910bdfd7c9d45f76cd3a44f3866d28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5084fa76fc215aa9d4a352c6fa9ce9a",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "5e469ea41c6b3e3b5b6d",
+    "url": "/react-deploy/static/css/main.7f20e45f.chunk.css"
+  },
+  {
+    "revision": "8689cddc0ef45fb77c2a",
+    "url": "/react-deploy/static/js/2.8b2f4158.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.8b2f4158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e469ea41c6b3e3b5b6d",
+    "url": "/react-deploy/static/js/main.ac41c066.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "a5e99ce770a9f818352c5b00e1be0790",
+    "url": "/react-deploy/static/media/icon_locationPin.a5e99ce7.png"
+  },
+  {
+    "revision": "488071d3f6873cce41af8da342b675d2",
+    "url": "/react-deploy/static/media/icon_search.488071d3.png"
+  }
+]);
